44795a8d0c353359fe732bc2a60862dd66fef068
Martin Petrov <martinmpetrov@hotmail.com>
2023-07-27 19:52:53 +0300

    Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,87 +3,87 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-// const configBuild = {
-//   index: {
-//     entry: resolve(__dirname, "./src/index.ts"),
-//     name: "index",
-//     fileName: "index",
-//   },
-//   hooks: {
-//     entry: resolve(__dirname, "./src/hooks/index.ts"),
-//     name: "hooks",
-//     fileName: "index",
-//   },
-//   themes: {
-//     entry: resolve(__dirname, "./src/themes/index.ts"),
-//     name: "themes",
-//     fileName: "index",
-//   },
-//   validators: {
-//     entry: resolve(__dirname, "./src/validators/index.ts"),
-//     name: "validators",
-//     fileName: "index",
-//   },
-// };
+const configBuild = {
+  index: {
+    entry: resolve(__dirname, "./src/index.ts"),
+    name: "index",
+    fileName: "index",
+  },
+  hooks: {
+    entry: resolve(__dirname, "./src/hooks/index.ts"),
+    name: "hooks",
+    fileName: "index",
+  },
+  themes: {
+    entry: resolve(__dirname, "./src/themes/index.ts"),
+    name: "themes",
+    fileName: "index",
+  },
+  validators: {
+    entry: resolve(__dirname, "./src/validators/index.ts"),
+    name: "validators",
+    fileName: "index",
+  },
+};
 
-// const currentBuild = configBuild[process.env.ENTRY];
+const currentBuild = configBuild[process.env.ENTRY];
 
-// if (currentBuild === undefined) {
-//   throw new Error("ENTRY is undefined or invalid");
-// }
+if (currentBuild === undefined) {
+  throw new Error("ENTRY is undefined or invalid");
+}
 
-// const configOutput = {
-//   index: "./dist",
-//   hooks: "./dist/hooks",
-//   themes: "./dist/themes",
-//   validators: "./dist/validators",
-// };
+const configOutput = {
+  index: "./dist",
+  hooks: "./dist/hooks",
+  themes: "./dist/themes",
+  validators: "./dist/validators",
+};
 
-// const currentOutput = configOutput[process.env.ENTRY];
+const currentOutput = configOutput[process.env.ENTRY];
 
-// if (currentOutput === undefined) {
-//   throw new Error("ENTRY is undefined or invalid");
-// }
-
-// export default defineConfig({
-//   plugins: [dts({ insertTypesEntry: true, skipDiagnostics: true }), react()],
-//   build: {
-//     outDir: currentOutput,
-//     lib: {
-//       ...currentBuild,
-//     },
-//     emptyOutDir: process.env.ENTRY === "index" ? true : false,
-//     rollupOptions: {
-//       external: ["react", "react-dom", "react/jsx-runtime"],
-//       output: {
-//         globals: {
-//           react: "React",
-//           "react-dom": "ReactDOM",
-//           "react/jsx-runtime": "jsxRuntime",
-//         },
-//       },
-//     },
-//   },
-// });
-
-// For running storybook, because of entry error
+if (currentOutput === undefined) {
+  throw new Error("ENTRY is undefined or invalid");
+}
 
 export default defineConfig({
-  plugins: [dts({ insertTypesEntry: true }), react()],
+  plugins: [dts({ insertTypesEntry: true, skipDiagnostics: true }), react()],
   build: {
+    outDir: currentOutput,
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "prismane",
-      fileName: "index",
+      ...currentBuild,
     },
+    emptyOutDir: process.env.ENTRY === "index" ? true : false,
     rollupOptions: {
       external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsxRuntime",
         },
       },
     },
   },
 });
+
+// For running storybook, because of entry error
+
+// export default defineConfig({
+//   plugins: [dts({ insertTypesEntry: true }), react()],
+//   build: {
+//     lib: {
+//       entry: resolve(__dirname, "src/index.ts"),
+//       name: "prismane",
+//       fileName: "index",
+//     },
+//     rollupOptions: {
+//       external: ["react", "react-dom", "react/jsx-runtime"],
+//       output: {
+//         globals: {
+//           react: "React",
+//           "react-dom": "ReactDOM",
+//         },
+//       },
+//     },
+//   },
+// });
