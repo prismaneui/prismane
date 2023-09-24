f318a48a2481bae46e59e10b4d16dcf7fb58a618
 <96326525+mutasim77@users.noreply.github.com>
2023-09-24 10:47:12 +0600

    Added new hook/useCopyToClipboard

---
+++ src/hooks/useCopyToClipboard/useCopyToClipboard.test.tsx
@@ -0,0 +1,35 @@
+import { renderHook } from '@testing-library/react-hooks/dom'
+import { describe, vi } from 'vitest';
+
+// Hook
+import useCopyToClipboard from './useCopyToClipboard'
+
+describe('useCopyToClipboard', () => {
+    const originalClipboard = { ...global.navigator.clipboard };
+
+    beforeEach(() => {
+        const mockClipboard = { writeText: vi.fn() };
+        // @ts-ignore mock clipboard
+        global.navigator.clipboard = mockClipboard;
+    });
+
+    afterEach(() => {
+        vi.resetAllMocks();
+        // @ts-ignore mock clipboard
+        global.navigator.clipboard = originalClipboard;
+    });
+
+    test('Should return null as initial copied value', () => {
+        const { result } = renderHook(() => useCopyToClipboard());
+
+        const [copiedValue] = result.current;
+        expect(copiedValue).toBeNull();
+    });
+
+    test('Should use clipboard', () => {
+        const { result } = renderHook(() => useCopyToClipboard())
+
+        expect(result.current[0]).toBe(null)
+        expect(typeof result.current[1]).toBe('function')
+    });
+})
