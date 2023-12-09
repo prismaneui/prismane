4ec32a521af46027428234f12faf130a969a5cdd
Martin Petrov <martinmpetrov@hotmail.com>
2023-12-10 01:20:46 +0200

    Migrate to vanilla CSS animations

--- src/components/Animation/Animation.tsx
+++ src/components/Animation/Animation.tsx
@@ -1,35 +1,44 @@
-import { forwardRef, useEffect, useState } from "react";
+import { forwardRef } from "react";
+import { keyframes } from "../../../stitches.config";
 // Components
-import Transition, { TransitionProps } from "../Transition/Transition";
+import Box, { BoxProps } from "../Box/Box";
 // Types
 import {
   PrismaneAnimations,
-  PrismaneTransitions,
   Versatile,
   PrismaneVersatile,
   PrismaneVersatileRef,
   PrismaneProps,
 } from "../../types";
 // Utils
-import { strip } from "../../utils";
+import { parse, strip } from "../../utils";
 
 export interface Animation {
-  in: object;
-  out: object;
+  in: any;
+  out: any;
 }
 
 export interface Animations {
-  [key: string]: Animation;
+  [key: string]: {
+    in: { (): string; name: string };
+    out: { (): string; name: string };
+  };
 }
 
 export type AnimationProps<E extends Versatile = "div"> = PrismaneVersatile<
   E,
   PrismaneProps<
     {
+      duration?: number;
+      delay?: number;
+      timing?: string;
+      count?: number | string;
+      direction?: string;
+      fill?: string;
       animation?: PrismaneAnimations | Animation;
       animated?: boolean;
     },
-    TransitionProps
+    BoxProps
   >
 >;
 
@@ -42,9 +51,12 @@ const Animation: AnimationComponent = forwardRef(
     {
       animation = "fade",
       transition,
-      duration,
-      delay,
+      duration = 150,
+      delay = 0,
       timing = "ease-in-out",
+      count = 1,
+      direction = "normal",
+      fill = "forwards",
       animated = true,
       className,
       sx,
@@ -54,111 +66,221 @@ const Animation: AnimationComponent = forwardRef(
     }: AnimationProps<E>,
     ref: PrismaneVersatileRef<E>
   ) => {
-    const [isAnimated, setIsAnimated] = useState(false);
-
-    useEffect(() => {
-      setIsAnimated(animated);
-    }, [animated]);
-
     const animations: Animations = {
       fade: {
-        in: { opacity: 1 },
-        out: { opacity: 0 },
+        in: keyframes({
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        }),
+        out: keyframes({
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        }),
       },
       scale: {
-        in: { transform: "scale(1)" },
-        out: { transform: "scale(0)" },
+        in: keyframes({
+          "0%": { transform: "scale(0)" },
+          "100%": { transform: "scale(1)" },
+        }),
+        out: keyframes({
+          "0%": { transform: "scale(1)" },
+          "100%": { transform: "scale(0)" },
+        }),
       },
       "scale-y": {
-        in: { transform: "scaleY(1)" },
-        out: { transform: "scaleY(0)" },
+        in: keyframes({
+          "0%": { transform: "scaleY(0)" },
+          "100%": { transform: "scaleY(1)" },
+        }),
+        out: keyframes({
+          "0%": { transform: "scaleY(1)" },
+          "100%": { transform: "scaleY(0)" },
+        }),
       },
       "scale-x": {
-        in: { transform: "scaleX(1)" },
-        out: { transform: "scaleX(0)" },
+        in: keyframes({
+          "0%": { transform: "scaleX(0)" },
+          "100%": { transform: "scaleX(1)" },
+        }),
+        out: keyframes({
+          "0%": { transform: "scaleX(1)" },
+          "100%": { transform: "scaleX(0)" },
+        }),
       },
       "skew-up": {
-        in: { transform: "skewY(0)" },
-        out: { transform: "skewY(-15deg)" },
+        in: keyframes({
+          "0%": { transform: "skewY(-15deg)" },
+          "100%": { transform: "skewY(0)" },
+        }),
+        out: keyframes({
+          "0%": { transform: "skewY(0)" },
+          "100%": { transform: "skewY(-15deg)" },
+        }),
       },
       "skew-down": {
-        in: { transform: "skewY(0)" },
-        out: { transform: "skewY(15deg)" },
+        in: keyframes({
+          "0%": { transform: "skewY(15deg)" },
+          "100%": { transform: "skewY(0)" },
+        }),
+        out: keyframes({
+          "0%": { transform: "skewY(0)" },
+          "100%": { transform: "skewY(15deg)" },
+        }),
       },
       "rotate-left": {
-        in: { transform: "rotate(0)" },
-        out: { transform: "rotate(-90deg)" },
+        in: keyframes({
+          "0%": { transform: "rotate(-90deg)" },
+          "100%": { transform: "rotate(0)" },
+        }),
+        out: keyframes({
+          "0%": { transform: "rotate(0)" },
+          "100%": { transform: "rotate(-90deg)" },
+        }),
       },
       "rotate-right": {
-        in: { transform: "rotate(0)" },
-        out: { transform: "rotate(90deg)" },
+        in: keyframes({
+          "0%": { transform: "rotate(90deg)" },
+          "100%": { transform: "rotate(0)" },
+        }),
+        out: keyframes({
+          "0%": { transform: "rotate(0)" },
+          "100%": { transform: "rotate(90deg)" },
+        }),
       },
       "slide-down": {
-        in: { opacity: 1, transform: "translateY(0)" },
-        out: { opacity: 0, transform: "translateY(100%)" },
+        in: keyframes({
+          "0%": { opacity: 0, transform: "translateY(100%)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        }),
+        out: keyframes({
+          "0%": { opacity: 1, transform: "translateY(0)" },
+          "100%": { opacity: 0, transform: "translateY(100%)" },
+        }),
       },
       "slide-up": {
-        in: { opacity: 1, transform: "translateY(0)" },
-        out: { opacity: 0, transform: "translateY(-100%)" },
+        in: keyframes({
+          "0%": { opacity: 0, transform: "translateY(-100%)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        }),
+        out: keyframes({
+          "0%": { opacity: 1, transform: "translateY(0)" },
+          "100%": { opacity: 0, transform: "translateY(-100%)" },
+        }),
       },
       "slide-left": {
-        in: { opacity: 1, transform: "translateX(0)" },
-        out: { opacity: 0, transform: "translateX(-100%)" },
+        in: keyframes({
+          "0%": { opacity: 0, transform: "translateX(-100%)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        }),
+        out: keyframes({
+          "0%": { opacity: 1, transform: "translateX(0)" },
+          "100%": { opacity: 0, transform: "translateX(-100%)" },
+        }),
       },
       "slide-right": {
-        in: { opacity: 1, transform: "translateX(0)" },
-        out: { opacity: 0, transform: "translateX(100%)" },
+        in: keyframes({
+          "0%": { opacity: 0, transform: "translateX(100%)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        }),
+        out: keyframes({
+          "0%": { opacity: 1, transform: "translateX(0)" },
+          "100%": { opacity: 0, transform: "translateX(100%)" },
+        }),
       },
       roll: {
-        in: { transform: "rotate(0deg)", opacity: 1 },
-        out: { transform: "rotate(-90deg)", opacity: 0 },
+        in: keyframes({
+          "0%": { transform: "rotate(-90deg)", opacity: 0 },
+          "100%": { transform: "rotate(0deg)", opacity: 1 },
+        }),
+        out: keyframes({
+          "0%": { transform: "rotate(0deg)", opacity: 1 },
+          "100%": { transform: "rotate(-90deg)", opacity: 0 },
+        }),
       },
       pulse: {
-        in: { transform: "scale(1)", opacity: 1 },
-        out: { transform: "scale(0.5)", opacity: 0 },
+        in: keyframes({
+          "0%": { transform: "scale(0.5)", opacity: 0 },
+          "100%": { transform: "scale(1)", opacity: 1 },
+        }),
+        out: keyframes({
+          "0%": { transform: "scale(1)", opacity: 1 },
+          "100%": { transform: "scale(0.5)", opacity: 0 },
+        }),
       },
       shake: {
-        in: { transform: "translateX(0)", opacity: 1 },
-        out: { transform: "translateX(-10px)", opacity: 0 },
+        in: keyframes({
+          "0%": { transform: "translateX(-10px)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        }),
+        out: keyframes({
+          "0%": { transform: "translateX(0)", opacity: 1 },
+          "100%": { transform: "translateX(-10px)", opacity: 0 },
+        }),
       },
       bounce: {
-        in: { transform: "translateY(20%)" },
-        out: { transform: "translateY(0)" },
+        in: keyframes({
+          "0%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-20%)" },
+          "100%": { transform: "translateY(0)" },
+        }),
+        out: keyframes({
+          "0%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-20%)" },
+          "100%": { transform: "translateY(0)" },
+        }),
       },
       flip: {
-        in: { transform: "rotateY(0deg)" },
-        out: { transform: "rotateY(180deg)" },
+        in: keyframes({
+          "0%": { transform: "rotateY(180deg)" },
+          "100%": { transform: "rotateY(0deg)" },
+        }),
+        out: keyframes({
+          "0%": { transform: "rotateY(0deg)" },
+          "100%": { transform: "rotateY(180deg)" },
+        }),
       },
     };
 
     const a =
-      typeof animation === "string" && animations[animation]
+      typeof animation === "string"
         ? animations[animation]
-        : (animation as Animation);
-
-    const animate = {
-      ...(isAnimated ? a?.in : a?.out),
-    };
+        : {
+            in: keyframes({
+              from: animation.out,
+              to: animation.in,
+            }),
+            out: keyframes({
+              from: animation.in,
+              to: animation.out,
+            }),
+          };
 
     const Component = as || "div";
 
+    const animate = {
+      animation: strip(
+        `${duration}ms ${timing} ${delay}ms ${count} ${direction} ${fill} ${
+          animated ? a.in : a.out
+        }`
+      ),
+    };
+
     return (
-      <Transition
+      <Box
         as={Component}
         className={strip(
           `${className ? className : ""} PrismaneAnimation-root`
         )}
-        transition={transition}
-        duration={duration}
-        delay={delay}
-        timing={timing}
-        sx={{ ...animate, ...sx }}
+        sx={{
+          ...animate,
+          ...sx,
+        }}
         ref={ref}
         data-testid="prismane-animation"
         {...props}
       >
         {children}
-      </Transition>
+      </Box>
     );
   }
 );
