105eb137305a65d721c087353db7dcfcbd983f69
Martin Petrov <martinpetrov404@gmail.com>
2024-03-15 18:10:32 +0200

    Spinner Component To Use Icon Component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -188,7 +188,15 @@ const Button: ButtonComponent = forwardRef(
         {...props}
       >
         {loading ? (
-          <Spinner />
+          <Spinner
+            size={variants(size, {
+              xs: fr(4.75),
+              sm: fr(4.75),
+              base: fr(5.25),
+              md: fr(6.25),
+              lg: fr(7.75),
+            })}
+          />
         ) : (
           <>
             {icon && (
