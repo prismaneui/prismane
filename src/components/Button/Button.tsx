7f53c34e42098d9316158dc26c624ad8f75a7373
Martin Petrov <80371253+spleafy@users.noreply.github.com>
2024-03-21 16:13:59 +0200

    Update Button.tsx
    Fix conflicts between branches in Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -204,14 +204,17 @@ const Button: ButtonComponent = forwardRef(
             {icon}
           </Icon>
         )}
-        {loading && <Spinner
+        {loading && (
+          <Spinner
             size={variants(size, {
               xs: fr(4.75),
               sm: fr(4.75),
               base: fr(5.25),
               md: fr(6.25),
               lg: fr(7.75),
-            })}}
+            })}
+          />
+        )}
         {children && (
           <Text
             className="PrismaneButton-text"
